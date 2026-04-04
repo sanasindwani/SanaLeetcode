bd47c5ce5dca9a4bee78803e9fe9de3d0ba227de
SanaSindwani <sanasindwani10@gmail.com>
2026-04-05 03:06:33 +0530

    Priority Queue(Min Heap) solution

---
+++ Heaps/kThLargestElementInArray.java
@@ -0,0 +1,17 @@
+package Heaps;
+
+import java.util.PriorityQueue;
+
+public class kThLargestElementInArray {
+    public int findKthLargest(int[] nums, int k) {
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i : nums){
+            pq.add(i);
+
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
